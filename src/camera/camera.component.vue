01fe035ea4fe594fe7e99e62cb05050d6abb7984
Florentin DUBOIS <contact@florentin-dubois.fr>
2018-01-01 14:21:29 +0100

    feat(camera): handle human error

--- src/camera/camera.component.vue
+++ src/camera/camera.component.vue
@@ -1,6 +1,11 @@
 <template>
   <div ref="container" class="container">
     <canvas ref="canvas"></canvas>
+
+    <md-snackbar md-position="center" :md-duration="duration" :md-active.sync="snackbar" md-persistent>
+      <span>La caméra est requise pour lire les qrcodes</span>
+      <md-button class="md-accent" @click="start()">Réessayer</md-button>
+    </md-snackbar>
   </div>
 </template>
 
@@ -13,46 +18,74 @@
 
 <script>
 import {Camera} from './camera.service'
+import {Scheduler} from 'rxjs'
 
 export default {
   name: 'Camera',
   data () {
     return {
-      video: document.createElement('video')
+      video: document.createElement('video'),
+      snackbar: false,
+      duration: 5000
     }
   },
 
   mounted () {
-    Camera
-      .stream({video: true})
-      .then(stream => {
-        const {video} = this
-
-        video.setAttribute('src', stream)
-        video.addEventListener('canplay', event => {
-          video.play()
-        })
-
-        requestAnimationFrame(this.refresh)
-      })
-      .catch(console.error)
-  },
-
-  methods: {
-    refresh () {
+    // wait after the component to be fully rendered
+    this.$nextTick(() => {
       const {video} = this
       const {canvas, container} = this.$refs
-      const context = canvas.getContext('2d')
 
       const width = container.clientWidth
       const height = container.clientHeight
 
+      video.setAttribute('width', width)
+      video.setAttribute('height', height)
+
       canvas.setAttribute('width', width)
       canvas.setAttribute('height', height)
+    })
 
-      context.drawImage(video, 0, 0, width, height)
+    this.start()
+  },
 
-      requestAnimationFrame(this.refresh)
+  beforeDestroy () {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  },
+
+  methods: {
+    refresh (vm) {
+      return function () {
+        const {video} = vm
+        const {canvas} = vm.$refs
+        const context = canvas.getContext('2d')
+
+        context.drawImage(video, 0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'))
+
+        this.schedule()
+      }
+    },
+
+    start () {
+      Camera
+        .stream({video: true})
+        .then(stream => {
+          const {video} = this
+
+          video.setAttribute('src', stream)
+          video.addEventListener('canplay', event => {
+            video.play()
+          })
+
+          this.subscription = Scheduler
+            .animationFrame
+            .schedule(this.refresh(this))
+        })
+        .catch(() => {
+          this.snackbar = true
+        })
     }
   }
 }
